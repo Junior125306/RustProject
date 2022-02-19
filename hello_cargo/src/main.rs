9d0c175caf1cb39ccc0aa2ddfa9e9bd5fed745d2
张振宇 <000000>
2022-02-19 12:11:41 +0800

    Day1基础

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,13 +1,32 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("猜数!");
-    println!("猜测一个数字!");
+    let secret_number = rand::thread_rng().gen_range(1..100);
 
-    let secret_number = rand::thread_rng().gen::<i32>();
     println!("神秘数字是{}", secret_number);
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("无法读取行");
-    println!("你猜测得数字是{}", guess)
+    loop {
+        println!("猜测一个数字!");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("无法读取行");
+
+        // shadow  去掉空格 解析成数字
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("你猜测得数字是{}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了"),
+            Ordering::Equal => {
+                println!("你赢了");
+                break;
+            }
+            Ordering::Greater => println!("太大了"),
+        }
+    }
 }
